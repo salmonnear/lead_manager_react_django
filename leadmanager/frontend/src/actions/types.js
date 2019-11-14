066b0005576df8f3e54c447eafe6beec68accc5b
salmonnear <samuelmennear@gmail.com>
2019-11-14 15:24:51 -0500

    start back at tut 3, 19:15

---
+++ leadmanager/frontend/src/actions/types.js
@@ -0,0 +1,17 @@
+export const GET_LEADS = "GET_LEADS";
+
+const initialState = {
+    leads: []
+}
+
+export default function(state= initialState, action) {
+    switch(action.type) {
+        case GET_LEADS:
+            return {
+                ...state,
+                leads: action.payload
+            }
+        default: 
+            return state;
+    }
+}
