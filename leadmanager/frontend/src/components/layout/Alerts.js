c1bca3b49cca91d2fce5e2053a948d28f9881734
Samuel Mennear <samuelmennear@gmail.com>
2019-12-15 12:48:52 -0500

    end of tut 4, alerts and error handling

--- leadmanager/frontend/src/components/layout/Alerts.js
+++ leadmanager/frontend/src/components/layout/Alerts.js
@@ -1,9 +1,30 @@
 import React, { Component, Fragment } from 'react';
 import { withAlert } from 'react-alert';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
 
 export class Alerts extends Component {
-    componentDidMount() {
-        this.props.alert.show('it works');
+    static propTypes = {
+        error: PropTypes.object.isRequired,
+        message: PropTypes.object.isRequired
+    }
+
+
+    componentDidUpdate(prevProps) {
+        const { error, alert, message } = this.props;
+        if (error !== prevProps.error) {
+
+            // using join here because comes in as array, this turns it into a string for output...
+            if (error.msg.name) alert.error(`Name: ${error.msg.name.join()}`);
+            if (error.msg.email) alert.error(`Email: ${error.msg.email.join()}`);
+            if (error.msg.message) alert.error(`Message: ${error.msg.message.join()}`);
+            //alert.error('There is an error');
+        }
+        if (message != prevProps.message) {
+            if(message.deleteLead) alert.success(message.deleteLead);
+            if(message.addLead) alert.success(message.addLead);
+        }
     }
 
     render() {
@@ -12,4 +33,12 @@ export class Alerts extends Component {
     }
 }
 
-export default withAlert()(Alerts);
+const mapStateToProps = state => ({
+    error: state.errors,
+    message: state.messages
+});
+
+
+export default connect(mapStateToProps)(withAlert()(Alerts));
+
+//export default withAlert()(Alerts);
