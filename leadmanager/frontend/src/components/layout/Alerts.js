73a000ee4623646490423ca72dd658a9e0201163
Samuel Mennear <samuelmennear@gmail.com>
2020-04-04 20:46:54 -0400

    finished vid 7

--- leadmanager/frontend/src/components/layout/Alerts.js
+++ leadmanager/frontend/src/components/layout/Alerts.js
@@ -19,11 +19,17 @@ export class Alerts extends Component {
             if (error.msg.name) alert.error(`Name: ${error.msg.name.join()}`);
             if (error.msg.email) alert.error(`Email: ${error.msg.email.join()}`);
             if (error.msg.message) alert.error(`Message: ${error.msg.message.join()}`);
-            //alert.error('There is an error');
+            if (error.msg.name) alert.error(`Name: ${error.msg.name.join()}`);
+            if (error.msg.non_field_errors) alert.error(error.msg.non_field_errors.join());
+            if (error.msg.username) alert.error(error.msg.username.join());
+            
         }
+
+
         if (message != prevProps.message) {
             if(message.deleteLead) alert.success(message.deleteLead);
             if(message.addLead) alert.success(message.addLead);
+            if(message.passwordNotMatch) alert.error(message.passwordNotMatch);
         }
     }
 
